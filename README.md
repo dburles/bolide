a31a76c8be6562215162ca1af05db1bf568dd54c
David Burles <dburles@gmail.com>
2013-08-23 20:45:28 +1000

    added helpers and coffee parameter

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Mostly based on the project structure from the unofficial Meteor FAQ.
 
 ## Usage
 ``` sh
-$ ./bolide.sh project_name
+$ ./bolide.sh project_name [coffee]
 ```
 
-
+Optional third parameter 'coffee' adds .coffee extension instead of the default .js
--- bolide.sh
+++ bolide.sh
@@ -2,6 +2,14 @@
 
 if [ "$1" ]
 then
+
+  if [ "$2" = "coffee" ]
+  then
+    lang="coffee"
+  else
+    lang="js"
+  fi
+
   meteor create $1
   cd $1
   rm *
@@ -10,12 +18,14 @@ then
   # client
   cd client
   mkdir stylesheets views lib
-  touch application.js
+  touch application.$lang
   touch template.html
+  cd lib
+  touch helpers.$lang
 
   # server
-  cd ../server
-  touch publications.js
+  cd ../../server
+  touch publications.$lang
   mkdir lib
 else
   echo "Usage: bolide [name]"
